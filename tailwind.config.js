4c7d1280a1f1391d9350b0ba526caaeace96093b
Long Phung <76096736+tronglongphung@users.noreply.github.com>
2022-02-18 12:46:27 +1030

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,44 @@
+const plugin = require('tailwindcss/plugin');
+
+module.exports = {
+  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}', './layouts/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        'qh-red': '#b9131a',
+      },
+    },
+    container: {
+      center: true,
+    },
+    screens: {
+      xs: '420px',
+      sm: '640px',
+      // => @media (min-width: 640px) { ... }
+
+      md: '768px',
+      // => @media (min-width: 768px) { ... }
+
+      lg: '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      xl: '1280px',
+      // => @media (min-width: 1280px) { ... }
+    },
+  },
+
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/line-clamp'),
+    require('@tailwindcss/typography'),
+
+    plugin(function ({ addVariant, e }) {
+      addVariant('not-first', ({ modifySelectors, separator }) => {
+        modifySelectors(({ className }) => {
+          return `.${e(`not-first${separator}${className}`)}:not(:first-child)`;
+        });
+      });
+    }),
+  ],
+};
