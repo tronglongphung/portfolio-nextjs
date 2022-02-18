d587274c764d04da1441b6e38c1841d389e29304
Tom Phung <tronglongphung@gmail.com>
2022-02-18 15:48:44 +1030

    styling changes

---
+++ pages/work.tsx
@@ -0,0 +1,10 @@
+import Head from 'next/head';
+import Image from 'next/image';
+
+export default function Work() {
+  return (
+    <>
+      <div className="flex items-center justify-center max-w-4xl gap-2 py-6">Work</div>
+    </>
+  );
+}
