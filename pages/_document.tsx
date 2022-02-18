f9bf6af8adb3bc83e11a43ee32cdc9343c4520b3
Tom Phung <tronglongphung@gmail.com>
2022-02-19 02:57:23 +1030

    add new font

---
+++ pages/_document.tsx
@@ -0,0 +1,26 @@
+import Document, { DocumentContext, Html, Head, Main, NextScript } from 'next/document';
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html lang="en" className="scroll-smooth">
+        <Head>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
