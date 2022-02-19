891f988c6faf27b445da737a0db65b64261f6c0f
Tom Phung <tronglongphung@gmail.com>
2022-02-20 00:49:30 +1030

    clean up

--- components/Card.tsx
+++ components/Card.tsx
@@ -5,7 +5,7 @@ export default function Card({ title, description, href, imageSrc, techs }) {
     <div className="flex flex-col justify-between h-full p-4 transition duration-300 border rounded dark:border-stone-600 dark:hover:border-stone-300 border-stone-200 hover:border-stone-400">
       <a href={href} target="_blank" rel="noopener noreferrer">
         <h3 className="mb-2 font-bold text-stone-800 dark:text-stone-200">{title}</h3>
-        <p className="mb-4 text-sm text-stone-600 dark:text-stone-400">{description}</p>
+        <p className="mb-4 text-sm text-stone-600 dark:text-stone-300">{description}</p>
       </a>
       <div>
         <div className="mb-4">
